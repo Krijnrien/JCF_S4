a53925fbcfeed4b012c3af7ac063d438e71ac2b1
Krijn van der Burg <k.vanderburg@student.fontys.nl>
2018-02-05 13:07:35 +0100

    Added string to word arraylist function. Use collections unique function to count words;

--- src/woordenapplicatie/gui/WoordenController.java
+++ src/woordenapplicatie/gui/WoordenController.java
@@ -7,9 +7,9 @@ package woordenapplicatie.gui;
  */
 
 
-
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -22,63 +22,77 @@ import javafx.scene.control.TextArea;
  * @author frankcoenen
  */
 public class WoordenController implements Initializable {
-    
-   private static final String DEFAULT_TEXT =   "Een, twee, drie, vier\n" +
-                                                "Hoedje van, hoedje van\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van papier\n" +
-                                                "\n" +
-                                                "Heb je dan geen hoedje meer\n" +
-                                                "Maak er één van bordpapier\n" +
-                                                "Eén, twee, drie, vier\n" +
-                                                "Hoedje van papier\n" +
-                                                "\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van, hoedje van\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van papier\n" +
-                                                "\n" +
-                                                "En als het hoedje dan niet past\n" +
-                                                "Zetten we 't in de glazenkas\n" +
-                                                "Een, twee, drie, vier\n" +
-                                                "Hoedje van papier";
-    
-    @FXML
-    private Button btAantal;
-    @FXML
-    private TextArea taInput;
-    @FXML
-    private Button btSorteer;
-    @FXML
-    private Button btFrequentie;
-    @FXML
-    private Button btConcordantie;
-    @FXML
-    private TextArea taOutput;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        taInput.setText(DEFAULT_TEXT);
-    }
-    
-    @FXML
-    private void aantalAction(ActionEvent event) {
-         throw new UnsupportedOperationException("Not supported yet."); 
-    }
+	private static final String DEFAULT_TEXT = "Een, twee, drie, vier\n" +
+			"Hoedje van, hoedje van\n" +
+			"Een, twee, drie, vier\n" +
+			"Hoedje van papier\n" +
+			"\n" +
+			"Heb je dan geen hoedje meer\n" +
+			"Maak er één van bordpapier\n" +
+			"Eén, twee, drie, vier\n" +
+			"Hoedje van papier\n" +
+			"\n" +
+			"Een, twee, drie, vier\n" +
+			"Hoedje van, hoedje van\n" +
+			"Een, twee, drie, vier\n" +
+			"Hoedje van papier\n" +
+			"\n" +
+			"En als het hoedje dan niet past\n" +
+			"Zetten we 't in de glazenkas\n" +
+			"Een, twee, drie, vier\n" +
+			"Hoedje van papier";
 
-    @FXML
-    private void sorteerAction(ActionEvent event) {
-         throw new UnsupportedOperationException("Not supported yet."); 
-    }
+	@FXML
+	private Button btAantal;
+	@FXML
+	private TextArea taInput;
+	@FXML
+	private Button btSorteer;
+	@FXML
+	private Button btFrequentie;
+	@FXML
+	private Button btConcordantie;
+	@FXML
+	private TextArea taOutput;
 
-    @FXML
-    private void frequentieAction(ActionEvent event) {
-         throw new UnsupportedOperationException("Not supported yet."); 
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		taInput.setText(DEFAULT_TEXT);
+	}
 
-    @FXML
-    private void concordatieAction(ActionEvent event) {
-         throw new UnsupportedOperationException("Not supported yet."); 
-    }
-   
+	@FXML
+	private void aantalAction(ActionEvent event) {
+
+		taOutput.setText(btAantal.getText());
+		Set<String> unique = new HashSet<String>(list);
+		for(String key : unique) {
+			System.out.println(key + ": " + Collections.frequency(list, key));
+		}
+
+
+	}
+
+	@FXML
+	private void sorteerAction(ActionEvent event) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@FXML
+	private void frequentieAction(ActionEvent event) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@FXML
+	private void concordatieAction(ActionEvent event) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+
+	private ArrayList<String> stringToWordList(String _taInput) {
+		ArrayList<String> wordArrayList = new ArrayList<String>();
+		wordArrayList.addAll(Arrays.asList(_taInput.split("\\s*(=>|,|\\s)\\s*")));
+		return wordArrayList;
+	}
+
 }
